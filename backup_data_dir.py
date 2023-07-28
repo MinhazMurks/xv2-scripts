feeb056d7891137ffa0447b95725b0cfdde2fc09
MinhazMurks <minhazabdullah1@gmail.com>
2023-07-27 22:19:40 -0400

    add xv2 install directory to backup

--- backup_data_dir.py
+++ backup_data_dir.py
@@ -2,7 +2,7 @@ import logging
 import os
 import shutil
 
-from common import data_directory, get_datetime, game_directory, setup_logger
+from common import data_directory, mod_data_directory, get_datetime, game_directory, setup_logger
 
 logger = setup_logger()
 
@@ -10,9 +10,11 @@ logger = setup_logger()
 def zip_data(src, zip_dir):
     if not os.path.exists(zip_dir):
         os.makedirs(zip_dir)
-    zip_file_path = os.path.join(zip_dir, f'data_backup_{get_datetime()}')
+    backslash_char = "\\"
+    zip_file_path = os.path.join(zip_dir, f'{src.split(backslash_char).pop()}_backup_{get_datetime()}')
     shutil.make_archive(zip_file_path, format='zip', root_dir=src, logger=logger)
 
 
 if __name__ == '__main__':
     zip_data(data_directory, os.path.join(game_directory, 'data_backups'))
+    zip_data(mod_data_directory, os.path.join(game_directory, 'data_backups'))
--- common.py
+++ common.py
@@ -5,20 +5,25 @@ import time
 char_dir = 'A:\\SteamLibrary\\steamapps\\common\\DB Xenoverse 2\\data\chara'
 game_directory = 'A:\\SteamLibrary\\steamapps\\common\\DB Xenoverse 2'
 data_directory = 'A:\\SteamLibrary\\steamapps\\common\\DB Xenoverse 2\\data'
+mod_data_directory = 'C:\\Users\\minha\\AppData\\Roaming\\XV2INS'
 char_slot_file = 'A:\\SteamLibrary\\steamapps\\common\\DB Xenoverse 2\\data\\XV2P_SLOTS.x2s'
 char_slot_backup_directory = 'A:\\SteamLibrary\\steamapps\\common\\DB Xenoverse 2\\data\\slot_backups'
-base_char_id_list = ['GOK', 'BDK', 'GK4', 'GOD', 'GKG', 'GTG', 'GHS', 'GHM', 'GHL', 'PIC', 'KLL', 'YMC', 'TSH', 'RAD',
-                     'SBM', 'NAP', 'VGT', 'VG4', 'GRD', 'BAT', 'RCM', 'JES', 'GNY', 'FRZ', 'FR4', 'FR5', 'TRX', 'TRS',
-                     'G17', 'S17', 'G18', 'CL3', 'CL4', 'CLJ', 'VDL', 'BUL', 'BUM', 'BUS', 'GTX', 'VTO', 'BRL', 'BLS',
-                     'PAN', 'GIL', 'OSV', 'OSB', 'OBB', 'SD3', 'SD4', 'SD1', 'GGT', 'STN', 'DMG', 'DM2', 'SIN', 'TOK',
-                     'BUZ', 'APL', 'RSB', 'GHP', 'MIR', 'TOW', 'WIS', 'HST', 'JCO', 'POD', 'FRG', 'STD', 'OSN', 'CL1',
-                     'TKT', 'TK3', 'ROK', 'GGK', 'GVG', 'GFR', 'G16', 'COL', 'CO2', 'GHF', 'TLS', 'JNB', 'FOF', 'TG1',
-                     'TG2', 'TG3', 'TG4', 'TG5', 'NP1', 'NP2', 'NP3', 'NP4', 'HUM', 'HUF', 'SYM', 'SYF', 'NMC', 'FRI',
-                     'MAM', 'MAF', 'MAP', 'TE0', 'TE1', 'EL0', 'TWP', 'TWT', 'THJ', 'THG', 'THC', 'THK', 'GG1', 'SLG',
-                     'ZBN', 'DDR', 'NIL', 'MRN', 'HIT', 'DND', 'SCR', 'BLM', 'JNG', 'GKB', 'TRF', 'FRS', 'CAB', 'HIK',
-                     'VDS', 'CMP', 'ZMS', 'GBR', 'BJK', 'VTB', 'ZMG', 'ZMD', 'BUU', 'DBR', 'G13', 'TPO', 'R17', 'JRN',
-                     'FUU', 'GKS', 'KFL', 'SB2', 'TM0', 'TM1', 'NI0', 'NI1', 'NI2', 'BLF', 'FOK', 'FOD', 'FOV', 'CL2',
-                     'URN', 'RSV', 'CPY', 'OWN', 'LPA', 'AVB', 'AVC', 'MST', 'PTN', 'XEN', 'XEG']
+base_char_id_list = ['GOK', 'BDK', 'GK4', 'GOD', 'GKG', 'GTG', 'GHS', 'GHM', 'GHL', 'PIC', 'KLL',
+                     'YMC', 'TSH', 'RAD', 'SBM', 'NAP', 'VGT', 'VG4', 'GRD', 'BAT', 'RCM', 'JES',
+                     'GNY', 'FRZ', 'FR4', 'FR5', 'TRX', 'TRS', 'G17', 'S17', 'G18', 'CL3', 'CL4',
+                     'CLJ', 'VDL', 'BUL', 'BUM', 'BUS', 'GTX', 'VTO', 'BRL', 'BLS', 'PAN', 'GIL',
+                     'OSV', 'OSB', 'OBB', 'SD3', 'SD4', 'SD1', 'GGT', 'STN', 'DMG', 'DM2', 'SIN',
+                     'TOK', 'BUZ', 'APL', 'RSB', 'GHP', 'MIR', 'TOW', 'WIS', 'HST', 'JCO', 'POD',
+                     'FRG', 'STD', 'OSN', 'CL1', 'TKT', 'TK3', 'ROK', 'GGK', 'GVG', 'GFR', 'G16',
+                     'COL', 'CO2', 'GHF', 'TLS', 'JNB', 'FOF', 'TG1*', 'TG2*', 'TG3*', 'TG4*',
+                     'TG5*', 'NP1', 'NP2', 'NP3', 'NP4', 'HUM', 'HUF', 'SYM', 'SYF', 'NMC', 'FRI',
+                     'MAM', 'MAF', 'MAP', 'TE0', 'TE1', 'EL0', 'TWP', 'TWT', 'THJ', 'THG', 'THC',
+                     'THK', 'GG1', 'SLG', 'ZBN', 'DDR', 'NIL', 'MRN', 'HIT', 'DND', 'SCR', 'BLM',
+                     'JNG', 'GKB', 'TRF', 'FRS', 'CAB', 'HIK', 'VDS', 'CMP', 'ZMS', 'GBR', 'BJK',
+                     'VTB', 'ZMG', 'ZMD', 'BUU', 'DBR', 'G13', 'TPO', 'R17', 'JRN', 'FUU', 'GKS',
+                     'KFL', 'SB2', 'TM0', 'TM1', 'NI0', 'NI1', 'NI2', 'BLF', 'FOK', 'FOD', 'FOV',
+                     'CL2', 'URN', 'FIO', 'FIT', 'FIG', 'SIG', 'SIP', 'SIO', 'SIM', 'SIH', 'RSV',
+                     'CPY', 'OWN', 'LPA', 'AVB', 'AVC', 'MST', 'PTN', 'XEN', 'XEG']
 installed_char_id_list = []
 empty_char_list = []
 
@@ -26,6 +31,7 @@ empty_char_list = []
 def get_datetime():
     timestamp = time.time()
     return datetime.datetime.fromtimestamp(timestamp).strftime('%m_%d_%H_%M')
+
 
 def setup_logger():
     logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
